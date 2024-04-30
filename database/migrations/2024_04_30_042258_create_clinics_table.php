475af9a33d24186e559c5ad57e6cc7af7187fa49
iramadani2003@gmail.com <iramadani2003@gmail.com>
2024-04-30 11:46:35 +0700

    first commit

---
+++ database/migrations/2024_04_30_042258_create_clinics_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clinics', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('symptom');
+            $table->string('diagnosis');
+            $table->string('drug');
+            $table->text('advice');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clinics');
+    }
+};
