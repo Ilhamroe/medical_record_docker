bce992e8eddfca909663a1d73009298f44f403b6
iramadani2003@gmail.com <iramadani2003@gmail.com>
2024-05-28 22:54:01 +0700

    push

--- app/Http/Controllers/Auth/AuthenticationController.php
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -70,8 +70,6 @@ class AuthenticationController extends Controller
 
         $identifier = $request->identifier;
         $user = User::where('email', $identifier)->orWhere('name', $identifier)->first();
-
-        $user = User::whereName($request->email)->first();
         if (!$user || !Hash::check($request->password, $user->password)) {
             return response([
                 'message' => 'Invalid Credentials'
